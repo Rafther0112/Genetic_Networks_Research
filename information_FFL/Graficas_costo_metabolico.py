9a3fdfaeb7ce6040817f19b6cc2527a055fa9603
Rafther0112 <r.velasquez@uniandes.edu.co>
2023-09-05 18:02:20 -0500

    actualizado

--- information_FFL/Graficas_costo_metabolico.py
+++ information_FFL/Graficas_costo_metabolico.py
@@ -5,6 +5,8 @@ import csv
 
 # %%
 simulacion_FFL_C1 = np.load('diccionario_FFL_C1.npy', allow_pickle=True).item()
-# %%
-simulacion_FFL_C1
+simulacion_FFL_C2 = np.load('diccionario_FFL_C2.npy', allow_pickle=True).item()
+simulacion_FFL_C3 = np.load('diccionario_FFL_C3.npy', allow_pickle=True).item()
+simulacion_FFL_C4 = np.load('diccionario_FFL_C4.npy', allow_pickle=True).item()
+
 # %%
